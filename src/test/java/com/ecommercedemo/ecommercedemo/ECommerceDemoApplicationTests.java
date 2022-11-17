d663d76d0ab09d623315a5efa1631ce9c586d957
serhatoluc <serhatoluc@hotmail.com>
2022-11-17 15:04:06 +0300

    eCommerce v1

---
+++ src/test/java/com/ecommercedemo/ecommercedemo/ECommerceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommercedemo.ecommercedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
